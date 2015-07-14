f2f7145aa94bd2193c112735f59517d0fa788347
Yass1408 <yassine.lakhdar.1@ulaval.ca>
2015-07-13 20:50:22 -0400

    prepared procedure

--- htdocs/xampp/inventory/newItemInventory.php
+++ htdocs/xampp/inventory/newItemInventory.php
@@ -1,53 +1,101 @@
 ﻿<!DOCTYPE html>
 <html>
-	<head>
-		<style>
-			table {
-				width: 100%;
-				border-collapse: collapse;
-			}
+<head>
+    <style>
+        table {
+            width: 100%;
+            border-collapse: collapse;
+        }
 
-			table, td, th {
-				border: 1px solid black;
-				padding: 5px;
-			}
+        table, td, th {
+            border: 1px solid black;
+            padding: 5px;
+        }
 
-			th {
-				text-align: left;
-			}
-		</style>
-	</head>
-	<body>
+        th {
+            text-align: left;
+        }
+    </style>
+</head>
+<body>
 
-		<?php
-		$upc = $_GET['upc'];
-		$servername = "localhost";
-		$username = "root";
-		$password = "";
-		$dbname = "eos";
+<?php
+$upc = $_GET['upc'];
+$store_id = 1;
 
-		// Create connection
-		$conn = new mysqli($servername, $username, $password, $dbname);
-		// Check connection
-		if ($conn -> connect_error) {
-			die("Connection failed: " . $conn -> connect_error);
-		}
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "eos";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
 
-			// Convert the query result into utf8
-			$conn->query("SET character_set_results=utf8");
+// Convert the query result into utf8
+$conn->query("SET character_set_results=utf8");
+/*
+$sql = "CALL sp_update_inventory('" . $upc . "', '1')";
+$result = $conn->query($sql);*/
+/*
+$stmt = $conn->prepare("CALL sp_update_inventory(?, ?)") or die($conn->error);
+$stmt->bind_param("si", $upc, $store_id) or die($stmt->error);
+$stmt->execute() or die($stmt->error);
+$stmt->bind_result($result);
+$stmt->fetch();
+*/
 
-			$sql = "CALL sp_update_inventory('" . $upc . "', '1')";
-            $result = $conn->query($sql);
+if ($stmt = $conn->prepare("CALL sp_update_inventory(?, ?)")) {
+
+    /* Binds variables to prepared statement */
+    $stmt->bind_param('si', $upc, $store_id);
+
+    /* execute query */
+    $stmt->execute();
+
+    if (!$result = $stmt->get_result()){
+        echo "item not in database.";
         
-	        if (!$result) {
-			 //the item is not in the database
-                echo "the item is not in the database";
+    } else {
+        echo '
+                    <table>
+                        <tr>
+                            <th>Item<br>Number</th>
+                            <th>Model</th>
+                            <th>Wholesale</th>
+                            <th>scaned<br>Quantity</th>
+                        </tr>';
+        //$stmt->bind_result($item_no, $model, $price, $qty);
+        while ($row = $result->fetch_assoc()) {
+            echo "<tr>";
+            echo "<td>" . $row['item_no'] . "</td>";
+            echo "<td>" . $row['model'] . "</td>";
+            echo "<td>" . $row['wholesale'] . "$</td>";
+            echo "<td>" . $row['scaned_qty'] . "</td>";
+            echo "</tr>";
+        }
+        echo "</table>";
+    }
 
+    /* free results */
+    $stmt->free_result();
 
-			} else {
+    /* close statement */
+    $stmt->close();
+}
 
-				echo '
+/*
+if (!$result) {
+    //the item is not in the database
+    echo "the item is not in the database";
+
+} else {
+
+    echo '
                     <table>
                         <tr>
                             <th>Item<br>Number</th>
@@ -56,20 +104,19 @@
                             <th>scaned<br>Quantity</th>
                         </tr>';
 
-				while ($row = $result->fetch_assoc()) {
-					echo "<tr>";
-					echo "<td>" . $row['item_no'] . "</td>";
-					echo "<td>" . $row['model'] . "</td>";
-					echo "<td>" . $row['wholesale'] . "$</td>";
-					echo "<td>" . $row['scaned_qty'] . "</td>";
-					echo "</tr>";
-				}
-				echo "</table>";
+    while ($row = $result->fetch_assoc()) {
+        echo "<tr>";
+        echo "<td>" . $row['item_no'] . "</td>";
+        echo "<td>" . $row['model'] . "</td>";
+        echo "<td>" . $row['wholesale'] . "$</td>";
+        echo "<td>" . $row['scaned_qty'] . "</td>";
+        echo "</tr>";
+    }
+    echo "</table>";
 
-				echo "<h1>table updated</h1>";
-			}
-
-		$conn -> close();
-		?>
-	</body>
+}
+*/
+$conn->close();
+?>
+</body>
 </html>
--- htdocs/xampp/inventory/refreshInventory.php
+++ htdocs/xampp/inventory/refreshInventory.php
@@ -1,36 +1,28 @@
 ﻿<!DOCTYPE html>
 <html>
-	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-		<script src="js/jquery-1.11.3.js"></script>
-		<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
-		<style>
-			table {
-				border-collapse: collapse;
-			}
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <script src="js/jquery-1.11.3.js"></script>
+    <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
+    <style>
+        table {
+            border-collapse: collapse;
+        }
 
-			table, td, th {
-				border: 1px solid black;
-				padding: 5px;
-			}
+        table, td, th {
+            border: 1px solid black;
+            padding: 5px;
+        }
 
-			th {
-				text-align: left;
-			}
-		</style>
-	</head>
+        th {
+            text-align: left;
+        }
+    </style>
+</head>
 
-	<body>
+<body>
 
-		<?php
-
-/*
-if (!($stmt = $mysqli->prepare("CALL sp_update_inventory(?, ?)"))) {
-    echo "Echec lors de la préparation : (" . $mysqli->errno . ") " . $mysqli->error;
-}
-
-$stmt->bind_param("ss", $upc, $store_id);
-*/
+<?php
 
 $servername = "localhost";
 $username = "root";
@@ -42,17 +34,17 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-} 
+}
 
 // Convert the query result into utf8
 $conn->query("SET character_set_results=utf8");
-
+/*
 mb_language('uni'); 
 mb_internal_encoding('UTF-8');
 $conn->query("set names 'utf8'");
+*/
 
-
-$sql="
+$sql = "
 SELECT 
     item_no, 
     model, 
@@ -74,8 +66,8 @@ echo '
             <th>Wholesale</th>
             <th>scaned<br>Quantity</th>
         </tr>';
-    
-while($row = $result->fetch_assoc()) {
+
+while ($row = $result->fetch_assoc()) {
     echo "<tr>";
     echo "<td>" . $row['item_no'] . "</td>";
     echo "<td>" . $row['model'] . "</td>";
@@ -89,62 +81,69 @@ $conn->close();
 echo "<br><br>connexion ended"
 ?>
 
-		<script>
-			function showUser(key, upc) {
-				var x = key.which || key.keyCode;
+<script>
+    function loadXMLDoc(url, cfunc) {
 
-				if (x == 13) {
-					if (upc == "") {
-						document.getElementById("inventoryTable-wrapper").innerHTML = "";
-						return;
-					} else {
-						if (window.XMLHttpRequest) {
-							// code for IE7+, Firefox, Chrome, Opera, Safari
-							xmlhttp = new XMLHttpRequest();
-						} else {
-							// code for IE6, IE5
-							xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-						}
-						xmlhttp.onreadystatechange = function() {
-							if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-								document.getElementById("inventoryTable-wrapper").innerHTML = xmlhttp.responseText;
-							}
-						}
-						xmlhttp.open("GET", "newItemInventory.php?upc=" + upc, true);
-						xmlhttp.send();
+        if (window.XMLHttpRequest) {
+            // code for IE7+, Firefox, Chrome, Opera, Safari
+            xmlhttp = new XMLHttpRequest();
+        } else {
+            // code for IE6, IE5
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        xmlhttp.onreadystatechange = cfunc;
+        xmlhttp.open("GET", url, true);
+        xmlhttp.send();
+    }
 
-						// reset input for next scan
-						document.getElementById("upc").value = "";
-					}
-				}
 
-				function addNewItem(item_no, model, wholesale, scaned_qty) {
-					var table = document.getElementById("inventoryTable");
-					var row = table.insertRow(-1);
+    function updateInventory(key, upc) {
+        var x = key.which || key.keyCode;
 
-					var cell0 = row.insertCell(0);
-					var cell1 = row.insertCell(1);
-					var cell2 = row.insertCell(2);
-					var cell3 = row.insertCell(3);
+        // if return is pressed
+        if (x == 13) {
 
-					cell0.innerHTML = item_no;
-					cell1.innerHTML = model;
-					cell2.innerHTML = wholesale;
-					cell3.innerHTML = scaned_qty;
-				}
+            // add the scanned item in the inventory
+            loadXMLDoc("newItemInventory.php?upc="+upc, function () {
+                if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                    document.getElementById("inventoryTable-wrapper").innerHTML = xmlhttp.responseText;
+                }
+            });
 
-			}
-		</script>
+            // reset input for next scan
+            document.getElementById("upcInput").value = "";
 
-		<p>
-			Scan new item in the input field below
-		</p>
-		<input id ="upc" type="text" name="upc" autofocus autocomplete="off" onkeypress="showUser(event, this.value)">
+        }
+    }
 
-		<br>
-		<div id="inventoryTable-wrapper">
-			<b>Person info will be listed here...</b>
-		</div>
+    function addNewItem(item_no, model, wholesale, scaned_qty) {
+        var table = document.getElementById("inventoryTable");
+        var row = table.insertRow(-1);
 
-	</body>
+        var cell0 = row.insertCell(0);
+        var cell1 = row.insertCell(1);
+        var cell2 = row.insertCell(2);
+        var cell3 = row.insertCell(3);
+
+        cell0.innerHTML = item_no;
+        cell1.innerHTML = model;
+        cell2.innerHTML = wholesale;
+        cell3.innerHTML = scaned_qty;
+    }
+
+</script>
+
+<p>
+    Scan new item in the input field below
+</p>
+<input id="upcInput" type="text" name="upc" autofocus autocomplete="off"
+       onkeypress="updateInventory(event, this.value)">
+
+<br>
+
+<div id="inventoryTable-wrapper">
+    <b>Person info will be listed here...</b>
+</div>
+
+</body>
 </html>
