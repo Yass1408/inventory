acef100da6ce1ad5c86200c678488708f5bd65e2
Yass1408 <yassine.lakhdar.1@ulaval.ca>
2015-07-16 00:02:07 -0400

    form linked with mysql modal button for editing scanned qty table hover

--- htdocs/xampp/inventory/newItem.php
+++ htdocs/xampp/inventory/newItem.php
@@ -1,13 +1,60 @@
 <!DOCTYPE html>
-    <html>
+<html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <script src="js/jquery-1.11.3.js"></script>
     <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
 </head>
 <body>
 
-<form class="form-horizontal">
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "eos";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Convert the query result into utf8
+$conn->query("SET character_set_results=utf8");
+
+
+function getOption($sql, $required = false)
+{
+    global $conn;
+    $result = $conn->query($sql);
+    echo '<option value="0">&nbsp;</option>';
+    $nbOption = 1;
+    while ($row = $result->fetch_array()) {
+        echo '<option value="' . $nbOption . '">' . $row[0] . '</option>';
+        $nbOption++;
+    }
+    if (!$required) {
+        echo '<option value="' . $nbOption . '">Other</option>';
+    }
+}
+
+function getBrand()
+{
+    getOption("SELECT DISTINCT brand FROM item where brand != '' ORDER BY brand", true);
+}
+
+function getColor()
+{
+    getOption("SELECT DISTINCT color FROM item where color != '' ORDER BY color");
+}
+
+function getPackage()
+{
+    getOption("SELECT DISTINCT package FROM item where package != '' ORDER BY package");
+}
+
+?>
+<form class="form-horizontal" id="new-item-form">
     <fieldset>
 
         <!-- Form Name -->
@@ -16,17 +63,20 @@
         <!-- Text input-->
         <div class="form-group">
             <label class="col-md-4 control-label" for="txtFldItemNo">Product Number</label>
-            <div class="col-md-4">
-                <input id="txtFldItemNo" name="txtFldItemNo" type="text" placeholder="" class="form-control input-md" required="">
 
+            <div class="col-md-4">
+                <input id="txtFldItemNo" name="txtFldItemNo" type="text" placeholder="" class="form-control input-md"
+                       autocomplete="off" required="">
             </div>
         </div>
 
         <!-- Text input-->
         <div class="form-group">
             <label class="col-md-4 control-label" for="txtFldModel">Model</label>
+
             <div class="col-md-4">
-                <input id="txtFldModel" name="txtFldModel" type="text" placeholder="" class="form-control input-md" required="">
+                <input id="txtFldModel" name="txtFldModel" type="text" placeholder="" class="form-control input-md"
+                       autocomplete="off" required="">
 
             </div>
         </div>
@@ -34,23 +84,20 @@
         <!-- Select Basic -->
         <div class="form-group">
             <label class="col-md-4 control-label" for="selectBrand">Brand</label>
+
             <div class="col-md-4">
                 <select id="selectBrand" name="selectBrand" class="form-control">
-                    <option value="1">Leatherman</option>
-                    <option value="2">Led Lenser</option>
+                    <?php getBrand() ?>
                 </select>
             </div>
         </div>
-
         <!-- Select Basic -->
         <div class="form-group">
             <label class="col-md-4 control-label" for="selectColor">Color</label>
+
             <div class="col-md-4">
                 <select id="selectColor" name="selectColor" class="form-control">
-                    <option value="0">--</option>
-                    <option value="1">Black</option>
-                    <option value="2">Blue</option>
-                    <option value="99">Other</option>
+                    <?php getColor() ?>
                 </select>
             </div>
         </div>
@@ -58,8 +105,11 @@
         <!-- Text input-->
         <div class="form-group">
             <label class="col-md-4 control-label" for="txtFldFeature">Feature</label>
+
             <div class="col-md-4">
-                <input id="txtFldFeature" name="txtFldFeature" type="text" placeholder="" class="form-control input-md">
+                <input id="txtFldFeature" name="txtFldFeature" type="text" placeholder=""
+                       class="form-control input-md"
+                       autocomplete="off">
 
             </div>
         </div>
@@ -67,8 +117,11 @@
         <!-- Text input-->
         <div class="form-group">
             <label class="col-md-4 control-label" for="txtFldSheath">Sheath</label>
+
             <div class="col-md-4">
-                <input id="txtFldSheath" name="txtFldSheath" type="text" placeholder="" class="form-control input-md">
+                <input id="txtFldSheath" name="txtFldSheath" type="text" placeholder=""
+                       class="form-control input-md"
+                       autocomplete="off">
 
             </div>
         </div>
@@ -76,13 +129,10 @@
         <!-- Select Basic -->
         <div class="form-group">
             <label class="col-md-4 control-label" for="selectPackage">Package</label>
+
             <div class="col-md-4">
                 <select id="selectPackage" name="selectPackage" class="form-control">
-                    <option value="1">BOX</option>
-                    <option value="2">PKG</option>
-                    <option value="3">PEG</option>
-                    <option value="4">GIFT</option>
-                    <option value="5">OTHER</option>
+                    <?php getPackage() ?>
                 </select>
             </div>
         </div>
@@ -90,8 +140,11 @@
         <!-- Text input-->
         <div class="form-group">
             <label class="col-md-4 control-label" for="txtFldPackQty">Package Quantity</label>
+
             <div class="col-md-4">
-                <input id="txtFldPackQty" name="txtFldPackQty" type="text" placeholder="" class="form-control input-md">
+                <input id="txtFldPackQty" name="txtFldPackQty" type="text" placeholder=""
+                       class="form-control input-md"
+                       autocomplete="off">
 
             </div>
         </div>
@@ -99,8 +152,10 @@
         <!-- Text input-->
         <div class="form-group">
             <label class="col-md-4 control-label" for="txtFldMsrp">MSRP</label>
+
             <div class="col-md-4">
-                <input id="txtFldMsrp" name="txtFldMsrp" type="text" placeholder="" class="form-control input-md">
+                <input id="txtFldMsrp" name="txtFldMsrp" type="text" placeholder="" class="form-control input-md"
+                       autocomplete="off">
 
             </div>
         </div>
@@ -108,8 +163,10 @@
         <!-- Text input-->
         <div class="form-group">
             <label class="col-md-4 control-label" for="txtFldUmp">UMP</label>
+
             <div class="col-md-4">
-                <input id="txtFldUmp" name="txtFldUmp" type="text" placeholder="" class="form-control input-md">
+                <input id="txtFldUmp" name="txtFldUmp" type="text" placeholder="" class="form-control input-md"
+                       autocomplete="off">
 
             </div>
         </div>
@@ -117,23 +174,28 @@
         <!-- Text input-->
         <div class="form-group">
             <label class="col-md-4 control-label" for="txtFldWholesale">Wholesale</label>
+
             <div class="col-md-4">
-                <input id="txtFldWholesale" name="txtFldWholesale" type="text" placeholder="" class="form-control input-md">
+                <input id="txtFldWholesale" name="txtFldWholesale" type="text" placeholder=""
+                       class="form-control input-md" autocomplete="off">
 
             </div>
         </div>
-
         <!-- Button -->
         <div class="form-group">
             <label class="col-md-4 control-label" for="btnNewItem">Save Item</label>
+
             <div class="col-md-4">
-                <button id="btnNewItem" name="btnNewItem" class="btn btn-inverse">Save</button>
+                <button id="btnNewItem" name="btnNewItem" class="btn btn-primary">Save</button>
             </div>
         </div>
 
     </fieldset>
 </form>
-
-
+<script src="js/jquery-1.11.3.js"></script>
 </body>
 </html>
+
+<?php
+$conn->close();
+?>
--- htdocs/xampp/inventory/refreshInventory.php
+++ htdocs/xampp/inventory/refreshInventory.php
@@ -2,8 +2,15 @@
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <script src="js/jquery-1.11.3.js"></script>
     <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
+    <script src="js/jquery-1.11.3.js"></script>
+
+    <!-- FOR PRODUCTION
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    -->
     <style>
         body {
             margin-top: 2%
@@ -15,6 +22,54 @@
 <body>
 <div class="container">
     <div class="row">
+
+        <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#newItemConfirm">Open Modal
+        </button>
+
+        <!-- Form Modal -->
+        <div id="newItemConfirm" class="modal fade" role="dialog">
+            <div class="modal-dialog modal-sm">
+
+                <!-- Form Modal content-->
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                        <h4 class="modal-title">Insert New Item</h4>
+                    </div>
+                    <div class="modal-body">
+                        <p>This item is not in de database.<br>Would you like to add it?</p>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">No</button>
+                        <button type="button" class="btn btn-primary">Yes</button>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+        <!-- Edit Item Quantity Modal -->
+        <div id="edit-item-qty" class="modal fade" role="dialog">
+            <div class="modal-dialog modal-sm">
+
+                <!-- Edit Item Quantity Modal content-->
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                        <h4 class="modal-title">Insert New Item</h4>
+                    </div>
+                    <div class="modal-body">
+                        <input id="new-item-qty" name="new-item-qty" type="number" placeholder="Item Quantity"
+                               class="form-control input-md"
+                               autocomplete="off" required="">
+                        <button type="button" class="btn btn-primary">Save</button>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+
         <div class="col-md-3">
             <form action="#" method="get">
                 <div class="input-group">
@@ -64,13 +119,14 @@ WHERE ITEM.upc = INVENTORY.upc";
 
         echo '
 <div class="col-md-9">
-    <table id="inventoryTable" class="table table-list-search">
+    <table id="inventoryTable" class="table table-list-search table-hover">
         <thead>
         <tr>
             <th>Item Number</th>
             <th>Model</th>
             <th>Wholesale</th>
             <th>Scaned Quantity</th>
+            <th>Edit</th>
         </tr>
         </thead>
         <tbody id="inventory-data">';
@@ -81,6 +137,7 @@ WHERE ITEM.upc = INVENTORY.upc";
             echo "<td>" . $row['model'] . "</td>";
             echo "<td>" . $row['wholesale'] . "$</td>";
             echo "<td>" . $row['scaned_qty'] . "</td>";
+            echo "<td><button class='btn btn-xs' data-toggle='modal' data-target='#edit-item-qty'><span class='glyphicon glyphicon-pencil'></span></button></td>";
             echo "</tr>";
         }
         echo "</tbody></table></div>";
@@ -88,113 +145,116 @@ WHERE ITEM.upc = INVENTORY.upc";
         $conn->close();
         ?>
 
-        <script>
-            var xmlhttp; // TODO: get rid of this global variable
-
-            function loadXMLDoc(url, cfunc) {
-                if (window.XMLHttpRequest) {
-                    // code for IE7+, Firefox, Chrome, Opera, Safari
-                    xmlhttp = new XMLHttpRequest();
-                } else {
-                    // code for IE6, IE5
-                    xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-                }
-                xmlhttp.onreadystatechange = cfunc;
-                xmlhttp.open("GET", url, true);
-                xmlhttp.send();
-            }
-
-            function updateInventory(key, upc) {
-                var x = key.which || key.keyCode;
-
-                // if return is pressed
-                if (x == 13) {
-
-                    // add the scanned item in the inventory
-                    loadXMLDoc("scanItem.php?upc=" + upc, function () {
-                        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-                            if (xmlhttp.responseText == "item_not_found") {
-                                alert("would you like to add the new item"); //TODO create a popup with new item info
-                            } else {
-                                document.getElementById("inventory-data").innerHTML = xmlhttp.responseText;
-                            }
-                        }
-                    });
-
-                    // reset input for next scan
-                    $("#txtFldupc").val("");
-
-                }
-            }
-
-            function addNewItem(item_no, model, wholesale, scaned_qty) {
-                var table = document.getElementById("inventoryTable");
-                var row = table.insertRow(-1);
-
-                var cell0 = row.insertCell(0);
-                var cell1 = row.insertCell(1);
-                var cell2 = row.insertCell(2);
-                var cell3 = row.insertCell(3);
-
-                cell0.innerHTML = item_no;
-                cell1.innerHTML = model;
-                cell2.innerHTML = wholesale;
-                cell3.innerHTML = scaned_qty;
-            }
-
-            /*Search for function
-             http://bootsnipp.com/snippets/featured/js-table-filter-simple-insensitive
-             */
-            $(document).ready(function () {
-                var activeSystemClass = $('.list-group-item.active');
-
-                //something is entered in search form
-                $('#system-search').keyup(function () {
-                    var that = this;
-                    // affect all table rows on in systems table
-                    var tableBody = $('.table-list-search tbody');
-                    var tableRowsClass = $('.table-list-search tbody tr');
-                    $('.search-sf').remove();
-                    tableRowsClass.each(function (i, val) {
-
-                        //Lower text for case insensitive
-                        var rowText = $(val).text().toLowerCase();
-                        var inputText = $(that).val().toLowerCase();
-                        if (inputText != '') {
-                            $('.search-query-sf').remove();
-                            tableBody.prepend('<tr class="search-query-sf"><td colspan="6"><strong>Searching for: "'
-                                + $(that).val()
-                                + '"</strong></td></tr>');
-                        }
-                        else {
-                            $('.search-query-sf').remove();
-                        }
-
-                        if (rowText.indexOf(inputText) == -1) {
-                            //hide rows
-                            tableRowsClass.eq(i).hide();
-
-                        }
-                        else {
-                            $('.search-sf').remove();
-                            tableRowsClass.eq(i).show();
-                        }
-                    });
-                    //all tr elements are hidden
-                    if (tableRowsClass.children(':visible').length == 0) {
-                        tableBody.append('<tr class="search-sf"><td class="text-muted" colspan="6">No entries found.</td></tr>');
-                    }
-                });
-            });
-
-        </script>
-
         <div class="col-md-3">
-            <input class="form-control" id="txtFldupc" name="upc" autofocus autocomplete="off" placeholder="Scan UPC here"
+            <input class="form-control" id="txtFldupc" name="upc" autofocus autocomplete="off"
+                   placeholder="Scan UPC here"
                    onkeypress="updateInventory(event, this.value)">
         </div>
     </div>
 </div>
 
+<!-- JS dependencies -->
+<script src="js/jquery-1.11.3.js"></script>
+<script src="bootstrap/js/bootstrap.js"></script>
+
+<!-- JS to put into a load event callback -->
+<script>
+    var xmlhttp; // TODO: get rid of this global variable
+
+    function loadXMLDoc(url, cfunc) {
+        if (window.XMLHttpRequest) {
+            // code for IE7+, Firefox, Chrome, Opera, Safari
+            xmlhttp = new XMLHttpRequest();
+        } else {
+            // code for IE6, IE5
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        xmlhttp.onreadystatechange = cfunc;
+        xmlhttp.open("GET", url, true);
+        xmlhttp.send();
+    }
+
+    function updateInventory(key, upc) {
+        var x = key.which || key.keyCode;
+
+        // if return is pressed
+        if (x == 13) {
+
+            // add the scanned item in the inventory
+            loadXMLDoc("scanItem.php?upc=" + upc, function () {
+                if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                    if (xmlhttp.responseText == "item_not_found") {
+                        alert("would you like to add the new item"); //TODO create a popup with new item info
+                    } else {
+                        document.getElementById("inventory-data").innerHTML = xmlhttp.responseText;
+                    }
+                }
+            });
+
+            // reset input for next scan
+            $("#txtFldupc").val("");
+
+        }
+    }
+
+    function addNewItem(item_no, model, wholesale, scaned_qty) {
+        var table = document.getElementById("inventoryTable");
+        var row = table.insertRow(-1);
+
+        var cell0 = row.insertCell(0);
+        var cell1 = row.insertCell(1);
+        var cell2 = row.insertCell(2);
+        var cell3 = row.insertCell(3);
+
+        cell0.innerHTML = item_no;
+        cell1.innerHTML = model;
+        cell2.innerHTML = wholesale;
+        cell3.innerHTML = scaned_qty;
+    }
+
+    /*Search function
+     http://bootsnipp.com/snippets/featured/js-table-filter-simple-insensitive
+     */
+    $(document).ready(function () {
+        //something is entered in search form
+        $('#system-search').keyup(function () {
+            var that = this;
+            // affect all table rows on in systems table
+            var tableBody = $('.table-list-search tbody');
+            var tableRowsClass = $('.table-list-search tbody tr');
+            $('.search-sf').remove();
+            tableRowsClass.each(function (i, val) {
+
+                //Lower text for case insensitive
+                var rowText = $(val).text().toLowerCase();
+                var inputText = $(that).val().toLowerCase();
+                if (inputText != '') {
+                    $('.search-query-sf').remove();
+                    tableBody.prepend('<tr class="search-query-sf"><td colspan="6"><strong>Searching for: "'
+                        + $(that).val()
+                        + '"</strong></td></tr>');
+                }
+                else {
+                    $('.search-query-sf').remove();
+                }
+
+                if (rowText.indexOf(inputText) == -1) {
+                    //hide rows
+                    tableRowsClass.eq(i).hide();
+
+                }
+                else {
+                    $('.search-sf').remove();
+                    tableRowsClass.eq(i).show();
+                }
+            });
+            //all tr elements are hidden
+            if (tableRowsClass.children(':visible').length == 0) {
+                tableBody.append('<tr class="search-sf"><td class="text-muted" colspan="6">No entries found.</td></tr>');
+            }
+        });
+    });
+
+</script>
 </body>
 </html>
